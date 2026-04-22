af46ce33a76c8994b00652524f499214cb6710df
shback215 <shbackup2252@gmail.com>
2026-04-22 11:43:31 -0400

    Update image files and optimize for web

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/booksreact/',
-  build: {
-    assetsDir: 'assets'
+  server: {
+    port: 9999
   }
 })
